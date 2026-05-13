8ab14a22dfc045ab0df3842ff50d48628d6de1ce
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-13 18:47:50 +0530

    dynamic routing

--- src/components/Netflix/NetflixShows.jsx
+++ src/components/Netflix/NetflixShows.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const NetflixShows = () => {
+
+  const showList = [
+    {id:1,name:"Money Heist"},
+    {id:2,name:"Breaking Bad"},
+    {id:3,name:"Stranger Things"},
+    {id:4,name:"Mirzapur"}
+  ]
+
   return (
     <div style={{textAlign:"center"}}>
         <h1>NetflixShows</h1>
+        {
+          showList.map((show)=>{
+            return <li>
+              <Link to={`/watch/${show.name}`}>{show.name}</Link>
+            </li>
+          })
+        }
     </div>
   )
 }
