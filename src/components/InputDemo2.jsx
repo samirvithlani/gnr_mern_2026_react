b15f5e3a48ff09be7d5098b937f94ef9820b9733
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-06 16:35:00 +0530

    hobbies

--- src/components/InputDemo2.jsx
+++ src/components/InputDemo2.jsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react'
 
 export const InputDemo2 = () => {
     const [gender, setgender] = useState("")
-    const [hobbies, sethobbies] = useState("")
+    const [hobbies, sethobbies] = useState([])
+    const hobbyHanlder =(event)=>{
+        
+        
+        console.log("event...",event)
+        if(event.target.checked){
+            sethobbies([...hobbies,event.target.value])
+        }
+        else{
+            //["a",b]==> a!=a b!=b -->false
+            sethobbies(hobbies.filter((h)=>h!=event.target.value))
+        }
+
+    }
   return (
     <div style={{textAlign:"center"}}>
         <h1>InputDemo2</h1>
@@ -15,12 +28,16 @@ export const InputDemo2 = () => {
         </div>
         <div>
             <label>Hobbies :</label>
-            Travel<input type='checkbox' name="hobbies" value='travel' onChange={(event)=>{sethobbies(event.target.value)}}></input>
-            Dacne<input type='checkbox' name="hobbies" value='dance' onChange={(event)=>{sethobbies(event.target.value)}}></input>
-            Sing<input type='checkbox' name="hobbies" value='sing' onChange={(event)=>{sethobbies(event.target.value)}}></input>
-            Scrolling<input type='checkbox' name="hobbies" value='scroll' onChange={(event)=>{sethobbies(event.target.value)}}></input>
+            Travel<input type='checkbox' name="hobbies" value='travel' onChange={(event)=>{hobbyHanlder(event)}}></input>
+            Dacne<input type='checkbox' name="hobbies" value='dance' onChange={(event)=>{hobbyHanlder(event)}}></input>
+            Sing<input type='checkbox' name="hobbies" value='sing' onChange={(event)=>{hobbyHanlder(event)}}></input>
+            Scrolling<input type='checkbox' name="hobbies" value='scroll' onChange={(event)=>{hobbyHanlder(event)}}></input>
             <br></br>
-            {hobbies}
+            {
+                hobbies.map((h)=>{
+                    return <li>{h}</li>
+                })
+            }
         </div>
 
     </div>
