da9134d8c8b3131938e2992134b82b5b5194bda3
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-10 18:46:49 +0530

    password

--- src/components/inputDemo3.jsx
+++ src/components/inputDemo3.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
+import { isElementOfType } from 'react-dom/test-utils'
 
 export const InputDemo3 = () => {
     const [date, setdate] = useState("")
+    const [password, setpassword] = useState("")
+
+    const isUpperCase = /[A-Z]/.test(password)
+    const checkLen = password.length>=8
+    const isLowerCase = /[a-z]/.test(password)
+    const isNumber = /[0-9]/.test(password)
+    const isspec = /[!@#$_]/.test(password)
+
     const getDateLable = (selectedDate)=>{
         if(!selectedDate)
             return "select date first"
@@ -36,9 +45,18 @@ export const InputDemo3 = () => {
         <div>
             <input type='date' onChange={(e)=>{setdate(e.target.value)}}></input>
         </div>
+        
         {
             getDateLable(date)
         }
+        <div>
+            <input type='text' onChange={(e)=>{setpassword(e.target.value)}}></input>
+        </div>
+        <p style={{color:isUpperCase?"green":"red"}}>min 1 cap</p>
+        <p style={{color:isLowerCase?"green":"red"}}>min 1 small</p>
+        <p style={{color:isNumber?"green":"red"}}>min 1 digit</p>
+        <p style={{color:checkLen?"green":"red"}}>length</p>
+        <p style={{color:isspec?"green":"red"}}>speccial char</p>
     </div>
   )
 }
